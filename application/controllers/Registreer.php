36c8f2d89c235addf3e10aafe5af7d9c10fa4f55
Tijmen <tijmenvandenpol@hotmail.com>
2016-06-09 15:17:45 +0200

    registreer in database gezet

--- application/controllers/Registreer.php
+++ application/controllers/Registreer.php
@@ -32,7 +32,7 @@ class Registreer extends CI_Controller
             array(
                 'field' => 'nickname',
                 'label' => 'Nickname',
-                'rules' => 'trim|required|min_length[3]|max_length[12]|is_unique[Persoon.nickname]',
+                'rules' => 'trim|xss_clean|required|min_length[3]|max_length[12]|is_unique[Persoon.nickname]',
                 'errors' => array(
                     'required' => 'U moet een %s invullen.',
                     'min_length' => 'Uw %s moet minstens 3 karakters zijn.',
@@ -53,9 +53,8 @@ class Registreer extends CI_Controller
             array(
                 'field' => 'tussenvoegsel',
                 'label' => 'Tussenvoegsel',
-                'rules' => 'trim|required|min_length[2]|max_length[12]',
+                'rules' => 'trim|min_length[2]|max_length[12]',
                 'errors' => array(
-                    'required' => 'U moet een %s invullen.',
                     'min_length' => 'Uw %s moet minstens 2 karakters zijn.',
                     'max_length' => 'Uw %s mag maximaal 12 karakters bevatten.',
                 ),
@@ -133,6 +132,165 @@ class Registreer extends CI_Controller
                     'required' => 'U moet een %s invullen.',
                 ),
             ),
+            array(
+                'field' => 'vraag1',
+                'label' => 'Vraag 1',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag2',
+                'label' => 'Vraag 2',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag2',
+                'label' => 'Vraag 2',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag3',
+                'label' => 'Vraag 3',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag4',
+                'label' => 'Vraag 4',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag5',
+                'label' => 'Vraag 5',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag6',
+                'label' => 'Vraag 6',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag7',
+                'label' => 'Vraag 7',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag8',
+                'label' => 'Vraag 8',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag9',
+                'label' => 'Vraag 9',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag10',
+                'label' => 'Vraag 10',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag11',
+                'label' => 'Vraag 11',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag12',
+                'label' => 'Vraag 12',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag13',
+                'label' => 'Vraag 13',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag14',
+                'label' => 'Vraag 14',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag15',
+                'label' => 'Vraag 15',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag16',
+                'label' => 'Vraag 16',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),array(
+                'field' => 'vraag17',
+                'label' => 'Vraag 17',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag18',
+                'label' => 'Vraag 18',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
+            array(
+                'field' => 'vraag19',
+                'label' => 'Vraag 19',
+                'rules' => 'trim|required',
+                'errors' => array(
+                    'required' => 'U moet %s invullen.',
+                ),
+            ),
         );
 
         $this->form_validation->set_rules($configuratie);
@@ -152,6 +310,143 @@ class Registreer extends CI_Controller
             }
             else
             {
+                $temp = $this->input->post('vraag1');
+                //initialisatie $tI
+                $tI = 50;
+                if ($temp=='-1')
+                    $tI-=10;
+                else if ($temp=='-2')
+                    $tI+=10;
+                $temp = $this->input->post('vraag2');
+                if ($temp=='-1')
+                    $tI-=10;
+                else if ($temp=='-2')
+                    $tI+=10;
+                $temp = $this->input->post('vraag3');
+                if ($temp=='-1')
+                    $tI-=10;
+                else if ($temp=='-2')
+                    $tI+=10;
+                $temp = $this->input->post('vraag4');
+                if ($temp=='-1')
+                    $tI-=10;
+                else if ($temp=='-2')
+                    $tI+=10;
+                $temp = $this->input->post('vraag5');
+                if ($temp=='-1')
+                    $tI-=10;
+                else if ($temp=='-2')
+                    $tI+=10;
+                //initialisatie $tN
+                $tN = 50;
+                $temp = $this->input->post('vraag6');
+                if ($temp=='-1')
+                    $tN+=12.5;
+                else if ($temp=='-2')
+                    $tN-=12.5;
+                $temp = $this->input->post('vraag7');
+                if ($temp=='-1')
+                    $tN+=12.5;
+                else if ($temp=='-2')
+                    $tN-=12.5;
+                $temp = $this->input->post('vraag8');
+                if ($temp=='-1')
+                    $tN+=12.5;
+                else if ($temp=='-2')
+                    $tN-=12.5;
+                $temp = $this->input->post('vraag9');
+                if ($temp=='-1')
+                    $tN+=12.5;
+                else if ($temp=='-2')
+                    $tN-=12.5;
+                //initialisatie $tT
+                $tT = 50;
+                $temp = $this->input->post('vraag10');
+                if ($temp=='-1')
+                    $tT+=12.5;
+                else if ($temp=='-2')
+                    $tT-=12.5;
+                $temp = $this->input->post('vraag11');
+                if ($temp=='-1')
+                    $tT+=12.5;
+                else if ($temp=='-2')
+                    $tT-=12.5;
+                $temp = $this->input->post('vraag12');
+                if ($temp=='-1')
+                    $tT+=12.5;
+                else if ($temp=='-2')
+                    $tT-=12.5;
+                $temp = $this->input->post('vraag13');
+                if ($temp=='-1')
+                    $tT+=12.5;
+                else if ($temp=='-2')
+                    $tT-=12.5;
+                //initialisatie $tJ
+                $tJ = 50;
+                $temp = $this->input->post('vraag14');
+                if ($temp=='-1')
+                    $tJ+=8.3333;
+                else if ($temp=='-2')
+                    $tJ-=8.3333;
+                $temp = $this->input->post('vraag15');
+                if ($temp=='-1')
+                    $tJ+=8.3333;
+                else if ($temp=='-2')
+                    $tJ-=8.3333;
+                $temp = $this->input->post('vraag16');
+                if ($temp=='-1')
+                    $tJ+=8.3333;
+                else if ($temp=='-2')
+                    $tJ-=8.3333;
+                $temp = $this->input->post('vraag17');
+                if ($temp=='-1')
+                    $tJ+=8.3333;
+                else if ($temp=='-2')
+                    $tJ-=8.3333;
+                $temp = $this->input->post('vraag18');
+                if ($temp=='-1')
+                    $tJ+=8.3333;
+                else if ($temp=='-2')
+                    $tJ-=8.3333;
+                $temp = $this->input->post('vraag19');
+                if ($temp=='-1')
+                    $tJ+=8.3333;
+                else if ($temp=='-2')
+                    $tJ-=8.3333;
+                $vI=abs($tI-100);
+                $vN=abs($tN-100);
+                $vT=abs($tT-100);
+                $vJ=abs($tJ-100);
+                $tv=' ';
+                if ($this->input->post('tussenvoegsel')!=NULL)
+                {
+                    $tv=' '.$this->input->post('tussenvoegsel').' ';
+                }
+                $persoondata = array(
+                    'nickname' => $this->input->post('nickname'),
+                    'naam' => $this->input->post('voornaam').$tv.$this->input->post('achternaam'),
+                    'wachtwoord' => $this->input->post('password'),
+                    'email' => $this->input->post('email'),
+                    'geboortedatum' => $this->input->post('geboortedatum'),
+                    'geslacht' => $this->input->post('geslacht'),
+                    'foto' => $this->input->post('foto'),
+                    'beschrijving' => $this->input->post('beschrijving'),
+                    'geslachtsvoorkeur' => $this->input->post('geslachtsvoorkeur'),
+                    'leeftijdsvoorkeurmin' => $this->input->post('minleeftijd'),
+                    'leeftijdsvoorkeurmax' => $this->input->post('maxleeftijd'),
+                    'tI' => floor($tI),
+                    'tN' => floor($tN),
+                    'tT' => floor($tT),
+                    'tJ' => floor($tJ),
+                    'vI' => floor($vI),
+                    'vN' => floor($vN),
+                    'vT' => floor($vT),
+                    'vJ' => floor($vJ)
+                );
+
+                $this->db->insert('Persoon', $persoondata);
+                $this->db->insert('Merkvoorkeur','brands[]');
+// Produces: INSERT INTO mytable (title, name, date) VALUES ('My title', 'My name', 'My date')
                 $this->load->view('templates/header');
                 $this->load->view('registreer/test');
                 $this->load->view('templates/footer');
--- application/views/registreer/registreer.php
+++ application/views/registreer/registreer.php
@@ -2,7 +2,8 @@
 
 <?php echo form_open('registreer/registreer');
 $this->load->helper('form');?>
-<div id="promotie">
+<div id="registreer">
+    <div id="promotie">
     <h2>Registratie</h2>
 
     <h5>Persoonsgegevens</h5>
@@ -29,12 +30,12 @@ $this->load->helper('form');?>
 
     <p>
         <label for="password">Wachtwoord</label>
-        <input type="password" name="password" value="<?php echo set_value('password'); ?>" size="50" />
+        <input type="password" name="password" value="" size="50" />
     </p>
 
     <p>
         <label for="passconf">Wachtwoord Bevestiging</label>
-        <input type="password" name="passconf" value="<?php echo set_value('passconf'); ?>" size="50" />
+        <input type="password" name="passconf" value="" size="50" />
     </p>
 
     <p>
@@ -80,7 +81,7 @@ $this->load->helper('form');?>
         <label for="maxleeftijd">Maximum leeftijd</label>
         <input id="maxleeftijd" type="number" name="maxleeftijd" value="<?php echo set_value('maxleeftijd'); ?>" size="50" min="18" step="1">
     </p>
-
+</div>
     <label>Ik heb een voorkeur voor deze merken</label>
     <div id="check_list">
         <input id="brand_CocaCola" type="checkbox" name="brands[]" value="CocaCola" <?php echo set_checkbox('brands[]', 'CocaCola'); ?>></label for="brand_CocaCola">CocaCola</label><br>
@@ -125,6 +126,162 @@ $this->load->helper('form');?>
         <input id="brand_AH" type="checkbox" name="brands[]" value="AH" <?php echo set_checkbox('brands[]', 'AH'); ?>><label for="brand_AH">AH</label><br>
     </div>
 
-    <div><input type="submit" value="Naar de test!" /></div>
+    <h2>Persoonlijkheidstest</h2>
+
+    <div>
+        <label>Vraag 1</label>
+        <div class="formulier">
+            <input id="vraag1-1" type="radio" name="vraag1" value="-1"<?php echo set_checkbox('vraag1', '-1'); ?>><label for="vraag1-1">Ik geef de voorkeur aan grote groepen mensen, met een grote diversiteit.</label><br>
+            <input id="vraag1-2" type="radio" name="vraag1" value="-2"<?php echo set_checkbox('vraag1', '-2'); ?>><label for="vraag1-2">Ik geef de voorkeur aan intieme bijeenkomsten met uitsluitend goede vrienden.</label><br>
+            <input id="vraag1-3" type="radio" name="vraag1" value="-3"<?php echo set_checkbox('vraag1', '-3'); ?>><label for="vraag1-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 2</label>
+        <div class="formulier">
+            <input id="vraag2-1" type="radio" name="vraag2" value="-1"<?php echo set_checkbox('vraag2', '-1'); ?>><label for="vraag2-1">Ik doe eerst, en dan denk ik.</label><br>
+            <input id="vraag2-2" type="radio" name="vraag2" value="-2"<?php echo set_checkbox('vraag2', '-2'); ?>><label for="vraag2-2">Ik denk eerst, en dan doe ik.</label><br>
+            <input id="vraag2-3" type="radio" name="vraag2" value="-3"<?php echo set_checkbox('vraag2', '-3'); ?>><label for="vraag2-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 3</label>
+        <div class="formulier">
+            <input id="vraag3-1" type="radio" name="vraag3" value="-1"<?php echo set_checkbox('vraag3', '-1'); ?>><label for="vraag3-1">Ik ben makkelijk afgeleid, met minder aandacht voor een enkele specifieke taak.</label><br>
+            <input id="vraag3-2" type="radio" name="vraag3" value="-2"<?php echo set_checkbox('vraag3', '-2'); ?>><label for="vraag3-2">Ik kan me goed focussen, met minder aandacht voor het grote geheel.</label><br>
+            <input id="vraag3-3" type="radio" name="vraag3" value="-3"<?php echo set_checkbox('vraag3', '-3'); ?>><label for="vraag3-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 4</label>
+        <div class="formulier">
+            <input id="vraag4-1" type="radio" name="vraag4" value="-1"<?php echo set_checkbox('vraag4', '-1'); ?>><label for="vraag4-1">Ik ben een makkelijke prater en ga graag uit.</label><br>
+            <input id="vraag4-2" type="radio" name="vraag4" value="-2"<?php echo set_checkbox('vraag4', '-2'); ?>><label for="vraag4-2">Ik ben een goede luisteraar en meer een privé-persoon.</label><br>
+            <input id="vraag4-3" type="radio" name="vraag4" value="-3"<?php echo set_checkbox('vraag4', '-3'); ?>><label for="vraag4-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 5</label>
+        <div class="formulier">
+            <input id="vraag5-1" type="radio" name="vraag5" value="-1"<?php echo set_checkbox('vraag5', '-1'); ?>><label for="vraag5-1">Als gastheer/-vrouw ben ik altijd het centrum van de belangstelling.</label><br>
+            <input id="vraag5-2" type="radio" name="vraag5" value="-2"<?php echo set_checkbox('vraag5', '-2'); ?>><label for="vraag5-2">Als gastheer/-vrouw ben altijd achter de schermen bezig om te zorgen dat alles soepeltjes verloopt.</label><br>
+            <input id="vraag5-3" type="radio" name="vraag5" value="-3"<?php echo set_checkbox('vraag5', '-3'); ?>><label for="vraag5-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 6</label>
+        <div class="formulier">
+            <input id="vraag6-1" type="radio" name="vraag6" value="-1"<?php echo set_checkbox('vraag6', '-1'); ?>><label for="vraag6-1">Ik geef de voorkeur aan concepten en het leren op basis van associaties.</label><br>
+            <input id="vraag6-2" type="radio" name="vraag6" value="-2"<?php echo set_checkbox('vraag6', '-2'); ?>><label for="vraag6-2">Ik geef de voorkeur aan observaties en het leren op basis van feiten.</label><br>
+            <input id="vraag6-3" type="radio" name="vraag6" value="-3"<?php echo set_checkbox('vraag6', '-3'); ?>><label for="vraag6-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 7</label>
+        <div class="formulier">
+            <input id="vraag7-1" type="radio"  name="vraag7" value="-1"<?php echo set_checkbox('vraag7', '-1'); ?>><label><label for="vraag7-1">Ik heb een groot inbeeldingsvermogen en heb een globaal overzicht van een project.</label><br>
+                <input id="vraag7-2" type="radio"  name="vraag7" value="-2"<?php echo set_checkbox('vraag7', '-2'); ?>><label><label for="vraag7-2">Ik ben pragmatisch ingesteld en heb een gedetailleerd beeld van elke stap.</label><br>
+                    <input id="vraag7-3" type="radio"  name="vraag7" value="-3"<?php echo set_checkbox('vraag7', '-3'); ?>><label><label for="vraag7-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 8</label>
+        <div class="formulier">
+            <input id="vraag8-1" type="radio" name="vraag8" value="-1"<?php echo set_checkbox('vraag8', '-1'); ?>><label for="vraag8-1">Ik kijk naar de toekomst.</label><br>
+            <input id="vraag8-2" type="radio" name="vraag8" value="-2"<?php echo set_checkbox('vraag8', '-2'); ?>><label for="vraag8-2">Ik houd mijn blik op het heden gericht.</label><br>
+            <input id="vraag8-3" type="radio" name="vraag8" value="-3"<?php echo set_checkbox('vraag8', '-3'); ?>><label for="vraag8-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 9</label>
+        <div class="formulier">
+            <input id="vraag9-1" type="radio" name="vraag9" value="-1"<?php echo set_checkbox('vraag9', '-1'); ?>><label for="vraag9-1">Ik houd van de veranderlijkheid in relaties en taken.</label><br>
+            <input id="vraag9-2" type="radio" name="vraag9" value="-2"<?php echo set_checkbox('vraag9', '-2'); ?>><label for="vraag9-2">Ik houd van de voorspelbaarheid in relaties en taken.</label><br>
+            <input id="vraag9-3" type="radio" name="vraag9" value="-3"<?php echo set_checkbox('vraag9', '-3'); ?>><label for="vraag9-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 10</label>
+        <div class="formulier">
+            <input id="vraag10-1" type="radio" name="vraag10" value="-1"<?php echo set_checkbox('vraag10', '-1'); ?>><label for="vraag10-1">Ik overdenk een beslissing helemaal.</label><br>
+            <input id="vraag10-2" type="radio" name="vraag10" value="-2"<?php echo set_checkbox('vraag10', '-2'); ?>><label for="vraag10-2">Ik beslis met mijn gevoel.</label><br>
+            <input id="vraag10-3" type="radio" name="vraag10" value="-3"<?php echo set_checkbox('vraag10', '-3'); ?>><label for="vraag10-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 11</label>
+        <div class="formulier">
+            <input id="vraag11-1" type="radio" name="vraag11" value="-1"<?php echo set_checkbox('vraag11', '-1'); ?>><label for="vraag11-1">Ik werk het beste met een lijst plussen en minnen.</label><br>
+            <input id="vraag11-2" type="radio" name="vraag11" value="-2"<?php echo set_checkbox('vraag11', '-2'); ?>><label for="vraag11-2">Ik beslis op basis van de gevolgen voor mensen.</label><br>
+            <input id="vraag11-3" type="radio" name="vraag11" value="-3"<?php echo set_checkbox('vraag11', '-3'); ?>><label for="vraag11-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 12</label>
+        <div class="formulier">
+            <input id="vraag12-1" type="radio" name="vraag12" value="-1"<?php echo set_checkbox('vraag12', '-1'); ?>><label for="vraag12-1">Ik ben van nature kritisch.</label><br>
+            <input id="vraag12-2" type="radio" name="vraag12" value="-2"<?php echo set_checkbox('vraag12', '-2'); ?>><label for="vraag12-2">Ik maak het mensen graag naar de zin.</label><br>
+            <input id="vraag12-3" type="radio" name="vraag12" value="-3"<?php echo set_checkbox('vraag12', '-3'); ?>><label for="vraag12-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 13</label>
+        <div class="formulier">
+            <input id="vraag13-1" type="radio" name="vraag13" value="-1"<?php echo set_checkbox('vraag13', '-1'); ?>><label for="vraag13-1">Ik ben eerder eerlijk dan tactisch.</label><br>
+            <input id="vraag13-2" type="radio" name="vraag13" value="-2"<?php echo set_checkbox('vraag13', '-2'); ?>><label for="vraag13-2">Ik ben eerder tactisch dan eerlijk.</label><br>
+            <input id="vraag13-3" type="radio" name="vraag13" value="-3"<?php echo set_checkbox('vraag13', '-3'); ?>><label for="vraag13-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 14</label>
+        <div class="formulier">
+            <input id="vraag14-1" type="radio" name="vraag14" value="-1"<?php echo set_checkbox('vraag14', '-1'); ?>><label for="vraag14-1">Ik houd van vertrouwde situaties.</label><br>
+            <input id="vraag14-2" type="radio" name="vraag14" value="-2"<?php echo set_checkbox('vraag14', '-2'); ?>><label for="vraag14-2">Ik houd van flexibele situaties.</label><br>
+            <input id="vraag14-3" type="radio" name="vraag14" value="-3"<?php echo set_checkbox('vraag14', '-3'); ?>><label for="vraag14-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 15</label>
+        <div class="formulier">
+            <input id="vraag15-1" type="radio" name="vraag15" value="-1"<?php echo set_checkbox('vraag15', '-1'); ?>><label for="vraag15-1">Ik plan alles, met een to-do lijstje in mijn hand.</label><br>
+            <input id="vraag15-2" type="radio" name="vraag15" value="-2"<?php echo set_checkbox('vraag15', '-2'); ?>><label for="vraag15-2">Ik wacht tot er meerdere ideeën opborrelen en kies dan on-the-fly wat te doen.</label><br>
+            <input id="vraag15-3" type="radio" name="vraag15" value="-3"<?php echo set_checkbox('vraag15', '-3'); ?>><label for="vraag15-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 16</label>
+        <div class="formulier">
+            <input id="vraag16-1" type="radio" name="vraag16" value="-1"<?php echo set_checkbox('vraag16', '-1'); ?>><label for="vraag16-1">Ik houd van het afronden van projecten.</label><br>
+            <input id="vraag16-2" type="radio" name="vraag16" value="-2"<?php echo set_checkbox('vraag16', '-2'); ?>><label for="vraag16-2">Ik houd van het opstarten van projecten.</label><br>
+            <input id="vraag16-3" type="radio" name="vraag16" value="-3"<?php echo set_checkbox('vraag16', '-3'); ?>><label for="vraag16-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 17</label>
+        <div class="formulier">
+            <input id="vraag17-1" type="radio" name="vraag17" value="-1"<?php echo set_checkbox('vraag17', '-1'); ?>><label for="vraag17-1">Ik ervaar stress door een gebrek aan planning en abrupte wijzigingen.</label><br>
+            <input id="vraag17-2" type="radio" name="vraag17" value="-2"<?php echo set_checkbox('vraag17', '-2'); ?>><label for="vraag17-2">Ik ervaar gedetailleerde plannen als benauwend en kijk uit naar veranderingen.</label><br>
+            <input id="vraag17-3" type="radio" name="vraag17" value="-3"<?php echo set_checkbox('vraag17', '-3'); ?>><label for="vraag17-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 18</label>
+        <div class="formulier">
+            <input id="vraag18-1" type="radio" name="vraag18" value="-1"<?php echo set_checkbox('vraag18', '-1'); ?>><label for="vraag18-1">Het is waarschijnlijker dat ik een doel bereik.</label><br>
+            <input id="vraag18-2" type="radio" name="vraag18" value="-2"<?php echo set_checkbox('vraag18', '-2'); ?>><label for="vraag18-2">Het is waarschijnlijker dat ik een kans zie.</label><br>
+            <input id="vraag18-3" type="radio" name="vraag18" value="-3"<?php echo set_checkbox('vraag18', '-3'); ?>><label for="vraag18-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+    <div>
+        <label>Vraag 19</label>
+        <div class="formulier">
+            <input id="vraag19-1" type="radio" name="vraag19" value="-1"<?php echo set_checkbox('vraag19', '-1'); ?>><label for="vraag19-1">"All play and no work maakt dat het project niet afkomt."</label><br>
+            <input id="vraag19-2" type="radio" name="vraag19" value="-2"<?php echo set_checkbox('vraag19', '-2'); ?>><label for="vraag19-2">"All work and no play maakt je maar een saaie pief."</label><br>
+            <input id="vraag19-3" type="radio" name="vraag19" value="-3"<?php echo set_checkbox('vraag19', '-3'); ?>><label for="vraag19-3">Ik zit er eigenlijk tussenin.</label>
+        </div>
+    </div>
+
+    <input type="submit" value="Registreer">
+
 </div>
 </form>
