1fd5afcfcf1682524addaf99380a0fbf845808ee
Nicole van den Dries <n.j.a.vandendries@students.uu.nl>
2016-06-11 17:57:35 +0200

    zonder errors

--- application/views/pages/home.php
+++ application/views/pages/home.php
@@ -23,7 +23,7 @@
             ->group_end()
             ->get();
 
-        $query2 = $this->db->select('CocaCola, Google')->from('Merkvoorkeur')
+        $query2 = $this->db->select('merk')->from('Merkvoorkeur')
             ->group_start()
             ->where('id', $id )
             ->group_end()
@@ -83,9 +83,9 @@
             echo substr($row->beschrijving, 0,50) . "...<br>";
             echo 'Merkvoorkeuren: ' ;
         }
-        foreach ($query2->result() as $row2)
-        {
-            echo  $row2->merk .', ';
+        foreach ($query2->result() as $row2) {
+            
+            echo ', ' . $row2->merk;
         }
 
         ?>
@@ -163,12 +163,11 @@
             echo "Geslacht: ".$row->geslacht . "<br>";
             echo "Type: ".$IE.$NS.$TF.$JP. "<br>";
             echo substr($row->beschrijving, 0,50) . "...<br>";
-            //echo 'Merkvoorkeuren: '.$row2->merk ."<br>";
-
-            foreach ($query2->result() as $row2){
-            echo 'Merkvoorkeuren: '.$row2->merk ."<br>";
-
-            }
+            echo 'Merkvoorkeuren: ';
+        }
+        foreach ($query2->result() as $row2)
+        {
+            echo  $row2->merk .', ';
         }
 
         ?>
@@ -186,7 +185,7 @@
             ->group_end()
             ->get();
 
-        $query2 = $this->db->select('CocaCola, Google')->from('Merkvoorkeur')
+        $query2 = $this->db->select('merk')->from('Merkvoorkeur')
             ->group_start()
             ->where('id', $id )
             ->group_end()
@@ -263,7 +262,7 @@
             ->group_end()
             ->get();
 
-        $query2 = $this->db->select('CocaCola, Google')->from('Merkvoorkeur')
+        $query2 = $this->db->select('merk')->from('Merkvoorkeur')
             ->group_start()
             ->where('id', $id )
             ->group_end()
@@ -308,89 +307,6 @@
             else{
                 $JP = 'P';
             }
-
-            $gbdatum = $row->geboortedatum;
-            $gbdatum=explode("-",$gbdatum);
-            $tempm = date("m");
-            $tempd = date("j");
-            $tempj = date("Y");
-            $leeftijd = $tempj - $gbdatum[0];
-
-
-            echo "Naam ". anchor('pages/mijngegevens/'.$id, $row->nickname) . "<br>";
-            echo "Leeftijd: ".$leeftijd."<br>";
-            echo "Geslacht: ".$row->geslacht . "<br>";
-            echo "Type: ".$IE.$NS.$TF.$JP. "<br>";
-            echo substr($row->beschrijving, 0,50) . "...<br>";
-            echo 'Merkvoorkeuren: ' . "<br>";
-            echo 'Merkvoorkeuren: ' ;
-        }
-        foreach ($query2->result() as $row2)
-        {
-            echo  $row2->merk .', ';
-        }
-
-        ?>
-
-    </div>
-    <div class="preview">
-        <?php
-
-        $max = $this->db->count_all_results('Persoon');
-        $id = rand (1, $max);
-        $query = $this->db->select('nickname, geboortedatum, geslacht, beschrijving, tI,tN, tT, tJ')->from('Persoon')
-
-            ->group_start()
-            ->where('id', $id )
-            ->group_end()
-            ->get();
-
-        $query2 = $this->db->select('CocaCola, Google')->from('Merkvoorkeur')
-            ->group_start()
-            ->where('id', $id )
-            ->group_end()
-            ->get();
-
-
-
-
-        foreach ($query->result() as $row)
-        {
-            if ($row->geslacht == 'm') {
-                echo anchor('pages/mijngegevens/'.$id,'<div class="thumbnail"> <img src="assets/img/man.jpg" id="thumbnail"></div>') ;
-            }
-            else{
-
-                echo anchor('pages/mijngegevens/'.$id,'<div class="thumbnail"> <img src="assets/img/vrouw.jpg" id="thumbnail"></div>');
-            }
-
-
-            if($row->tI > 50){
-                $IE = 'I';
-            }
-            else{
-                $IE = 'E';
-            }
-
-            if($row->tN > 50){
-                $NS = 'N';
-            }
-            else{
-                $NS = 'S';
-            }
-            if($row->tT > 50){
-                $TF = 'T';
-            }
-            else{
-                $TF = 'F';
-            }
-            if($row->tJ > 50){
-                $JP = 'J';
-            }
-            else{
-                $JP = 'P';
-            }
-
 
             $gbdatum = $row->geboortedatum;
             $gbdatum=explode("-",$gbdatum);
@@ -415,6 +331,88 @@
         ?>
 
     </div>
+    <div class="preview">
+        <?php
+
+        $max = $this->db->count_all_results('Persoon');
+        $id = rand (1, $max);
+        $query = $this->db->select('nickname, geboortedatum, geslacht, beschrijving, tI,tN, tT, tJ')->from('Persoon')
+
+            ->group_start()
+            ->where('id', $id )
+            ->group_end()
+            ->get();
+
+        $query2 = $this->db->select('merk')->from('Merkvoorkeur')
+            ->group_start()
+            ->where('id', $id )
+            ->group_end()
+            ->get();
+
+
+
+
+        foreach ($query->result() as $row)
+        {
+            if ($row->geslacht == 'm') {
+                echo anchor('pages/mijngegevens/'.$id,'<div class="thumbnail"> <img src="assets/img/man.jpg" id="thumbnail"></div>') ;
+            }
+            else{
+
+                echo anchor('pages/mijngegevens/'.$id,'<div class="thumbnail"> <img src="assets/img/vrouw.jpg" id="thumbnail"></div>');
+            }
+
+
+            if($row->tI > 50){
+                $IE = 'I';
+            }
+            else{
+                $IE = 'E';
+            }
+
+            if($row->tN > 50){
+                $NS = 'N';
+            }
+            else{
+                $NS = 'S';
+            }
+            if($row->tT > 50){
+                $TF = 'T';
+            }
+            else{
+                $TF = 'F';
+            }
+            if($row->tJ > 50){
+                $JP = 'J';
+            }
+            else{
+                $JP = 'P';
+            }
+
+
+            $gbdatum = $row->geboortedatum;
+            $gbdatum=explode("-",$gbdatum);
+            $tempm = date("m");
+            $tempd = date("j");
+            $tempj = date("Y");
+            $leeftijd = $tempj - $gbdatum[0];
+
+
+            echo "Naam ". anchor('pages/mijngegevens/'.$id, $row->nickname) . "<br>";
+            echo "Leeftijd: ".$leeftijd."<br>";
+            echo "Geslacht: ".$row->geslacht . "<br>";
+            echo "Type: ".$IE.$NS.$TF.$JP. "<br>";
+            echo substr($row->beschrijving, 0,50) . "...<br>";
+            echo 'Merkvoorkeuren: ' ;
+        }
+        foreach ($query2->result() as $row2)
+        {
+            echo  $row2->merk .', ';
+        }
+
+        ?>
+
+    </div>
 
     <div class="preview">
         <?php
@@ -428,7 +426,7 @@
             ->group_end()
             ->get();
 
-        $query2 = $this->db->select('CocaCola, Google')->from('Merkvoorkeur')
+        $query2 = $this->db->select('merk')->from('Merkvoorkeur')
             ->group_start()
             ->where('id', $id )
             ->group_end()
@@ -486,7 +484,7 @@
             echo "Geslacht: ".$row->geslacht . "<br>";
             echo "Type: ".$IE.$NS.$TF.$JP. "<br>";
             echo substr($row->beschrijving, 0,50) . "...<br>";
-            echo 'Merkvoorkeuren: ' . "<br>";
+
             echo 'Merkvoorkeuren: ' ;
         }
         foreach ($query2->result() as $row2)
